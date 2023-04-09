fa9ee61a8ad7315ebca29ed6f24958a70a032597
David Hurtado <davidhuertado@gmail.com>
2023-04-09 12:02:58 +0200

    feat persistence

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '@/styles/globals.css';
+import { PersistGate } from 'redux-persist/integration/react';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
-import store from '@/store';
+import { store, persistor } from '@/store';
 import { Noto_Sans_Display } from 'next/font/google';
 
 const noto = Noto_Sans_Display({ subsets: ['latin'] });
@@ -9,12 +10,14 @@ const noto = Noto_Sans_Display({ subsets: ['latin'] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <style jsx global>{`
-        html {
-          font-family: ${noto.style.fontFamily};
-        }
-      `}</style>
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <style jsx global>{`
+          html {
+            font-family: ${noto.style.fontFamily};
+          }
+        `}</style>
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
--- store/index.ts
+++ store/index.ts
@@ -1,15 +1,35 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import recipesSlice from './recipesSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
-const store = configureStore({
-  reducer: {
-    recipes: recipesSlice,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  recipes: recipesSlice,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const makeStore = () =>
+  configureStore({
+    reducer: persistedReducer,
+    devTools: true,
+  });
+
+// const store = configureStore({
+//   reducer: {
+//     recipes: recipesSlice,
+//   },
+// });
+
+export const store = makeStore();
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
-
-export default store;
